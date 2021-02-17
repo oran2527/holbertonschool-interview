b8e245587a1022474b093dba51677cc25318c594
oran2527 <orlago250183@gmail.com>
2021-02-17 11:37:48 +0000

    doing the other way

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,34 +5,15 @@
 def minOperations(n):
     """process to know how many steps it is required for printing n char"""
 
-    l = []
-    cn = n
-    add = ''
-    cont = 1
-
     if n <= 1:
         return 0
+    div = 2
+    no = 0
+    while n > 1:
+        if n % div == 0:
+            n = n / div
+            no = no + div
+        else:
+            div = div + 1
+    return no
 
-    if n % 2 == 0:
-        while (cn > 1):
-            l.append(int(cn / 2))
-            cn = int(cn / 2)
-
-    if n % 3 == 0:
-        while (cn > 1):
-            l.append(int(cn / 3))
-            cn = int(cn / 3)
-
-    if n % 2 != 0 and n % 3 != 0:
-        return n
-
-    text = 'H'
-
-    while len(text) < n:
-        if len(text) in l:
-            add = str(text)
-            cont = cont + 1
-        text = text + add
-        cont = cont + 1    
-
-    return cont - 1
