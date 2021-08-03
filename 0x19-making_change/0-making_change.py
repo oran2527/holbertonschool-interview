035556383e582af6040cf90c8628df8a29e6da57
oran2527 <orlago250183@gmail.com>
2021-08-03 19:24:45 +0000

    fewest number of coins needed to meet a given amount total

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Making change """
+
+
+def makeChange(coins, total):
+    """ Making change """
+    
+    if total <= 0:
+        return 0
+    coins_dic = {}
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        if total / coin >= 1:
+            coins_dic[coin] = int(total / coin)
+            total = total % coin
+        if total == 0:
+            break
+    if total != 0:
+        return -1
+    return sum(coins_dic.values())
